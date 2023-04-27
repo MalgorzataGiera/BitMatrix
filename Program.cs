97c65fd0f5562435b177ff84193b9a6ce0d111d8
unknown <gosgie02@wp.pl>
2023-04-27 23:25:51 +0200

    ToString() overloaded

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// konstruktor BitMatrix(bool[,])
+bool[,] arr = new bool[,] { { true, false, true }, { false, true, true } };
+var m = new BitMatrix(arr);
+Console.WriteLine(arr.GetLength(0) == m.NumberOfRows);
+Console.WriteLine(arr.GetLength(1) == m.NumberOfColumns);
+Console.Write(m.ToString());
