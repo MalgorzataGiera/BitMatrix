c0898f99eb73f2e9a9706110e36d049569dec0be
unknown <gosgie02@wp.pl>
2023-04-27 18:44:06 +0200

    ToString() overloaded

---
+++ BitMatrixToString.cs
@@ -0,0 +1,14 @@
+﻿public partial class BitMatrix
+{
+    public override string ToString()
+    {
+        string result = "";
+        for (int i = 0; i < NumberOfRows; i++)
+        {
+            for (int j = 0; j < NumberOfColumns; j++)
+                result += (BoolToBit(data[i * j + j]));
+            result += (Environment.NewLine);
+        }
+        return result;
+    }
+}
